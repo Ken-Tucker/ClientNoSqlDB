58885134a5e4e616d8e0e918a8734a3d1bf94cb8
Ken Tucker <vb2ae@live.com>
2018-12-15 07:27:38 -0500

    Changed storage to use system.io instead of pinvoke

--- ClientNoSqlDB/Storage/FileSystem/DbTableStorage.cs
+++ ClientNoSqlDB/Storage/FileSystem/DbTableStorage.cs
@@ -5,289 +5,288 @@ using System.Threading;
 
 namespace ClientNoSqlDB.FileSystem
 {
-  class DbTableStorage : IDbTableStorage
-  {
-    readonly string _indexName;
-    readonly string _dataName;
-
-    public DbTableStorage(string path, string name)
+    class DbTableStorage : IDbTableStorage
     {
-      _indexName = Path.Combine(path, name + ".index");
-      _dataName = Path.Combine(path, name + ".data");
-    }
+        readonly string _indexName;
+        readonly string _dataName;
 
-    readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
-
-    public void Flush()
-    {
-    }
-
-    const int BufferSize = 256 * 1024; // 256K
-
-    Stream OpenRead(string name, bool buffered = false)
-    {
-      for (int i = 0; i < 10; i++)
-        try
+        public DbTableStorage(string path, string name)
         {
-
-          return new FileStream(name, FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read, buffered ? BufferSize : 4 * 1024);
-        }
-        catch
-        {
-          Thread.Sleep(100);
+            _indexName = Path.Combine(path, name + ".index");
+            _dataName = Path.Combine(path, name + ".data");
         }
 
-      throw new IOException("Cannot aquire read lock");
-    }
+        readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
 
-
-    Stream OpenWrite(string name, bool buffered = false)
-    {
-      for (int i = 0; i < 10; i++)
-        try
+        public void Flush()
         {
-          return new FileStream(name, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, buffered ? BufferSize : 4 * 1024);
-        }
-        catch
-        {
-          Thread.Sleep(100);
         }
 
-      throw new IOException("Cannot aquire read lock");
-    }
+        const int BufferSize = 256 * 1024; // 256K
 
-
-    public IDbTableReader BeginRead()
-    {
-      _lock.EnterReadLock();
-      try
-      {
-        return new Reader(this, _lock.ExitReadLock);
-      }
-      catch
-      {
-        _lock.ExitReadLock();
-        throw;
-      }
-    }
-
-    public IDbTableWriter BeginWrite()
-    {
-      _lock.EnterWriteLock();
-      try
-      {
-        return new Writer(this, _lock.ExitWriteLock);
-      }
-      catch
-      {
-        _lock.ExitWriteLock();
-        throw;
-      }
-    }
-
-    public IDbTableWriter BeginCompact()
-    {
-      _lock.EnterWriteLock();
-      try
-      {
-        return new Compacter(this, _lock.ExitWriteLock);
-      }
-      catch
-      {
-        _lock.ExitWriteLock();
-        throw;
-      }
-    }
-
-    class Reader : IDbTableReader
-    {
-      protected Stream _readStream, _indexStream;
-      protected readonly DbTableStorage _table;
-      readonly Action _finalizer;
-      internal DateTimeOffset _ts;
-
-      public Reader(DbTableStorage table, Action finalizer)
-      {
-        _table = table;
-        _finalizer = finalizer;
-        CreateStreams();
-        UpdateTs();
-      }
-
-      protected void UpdateTs()
-      {
-        _ts = File.GetLastWriteTime(_table._indexName);
-      }
-
-      protected virtual void CreateStreams()
-      {
-        _indexStream = _table.OpenRead(_table._indexName);
-        try
+        Stream OpenRead(string name, bool buffered = false)
         {
-          _readStream = _table.OpenRead(_table._dataName, true);
+            for (int i = 0; i < 10; i++)
+                try
+                {
+                    return new FileStream(name, FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read, buffered ? BufferSize : 4 * 1024);
+                }
+                catch
+                {
+
+                    Thread.Sleep(100);
+                }
+
+            throw new IOException("Cannot aquire read lock");
         }
-        catch
+
+        Stream OpenWrite(string name, bool buffered = false)
         {
-          _indexStream.Dispose();
-          throw;
+            for (int i = 0; i < 10; i++)
+                try
+                {
+                    return new FileStream(name, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, buffered ? BufferSize : 4 * 1024);
+                }
+                catch
+                {
+                    Thread.Sleep(100);
+                }
+
+            throw new IOException("Cannot aquire read lock");
         }
-      }
 
-      public DateTimeOffset Ts { get { return _ts; } }
 
-      public DbTableInfo GetInfo()
-      {
-        return new DbTableInfo
+        public IDbTableReader BeginRead()
         {
-          DataSize = _readStream.Length,
-          IndexSize = _indexStream.Length
-        };
-      }
-
-      public byte[] ReadIndex()
-      {
-        int len = (int)_indexStream.Length;
-        if (len == 0)
-          return null;
-
-        return ReadStream(_indexStream, len);
-      }
-
-      protected static byte[] ReadStream(Stream stream, int len)
-      {
-        var buffer = new byte[len];
-
-        var pos = 0;
-        while (pos < len)
-          pos += stream.Read(buffer, pos, len - pos);
-
-        return buffer;
-      }
-
-      public byte[] ReadData(long position, int length)
-      {
-        if (_readStream.Position != position)
-          _readStream.Seek(position, SeekOrigin.Begin);
-
-        return ReadStream(_readStream, length);
-      }
-
-      public virtual void Dispose()
-      {
-        _readStream.Dispose();
-        _indexStream.Dispose();
-
-        _finalizer();
-      }
-    }
-
-    class Writer : Reader, IDbTableWriter
-    {
-      Stream _writeStream;
-
-      public Writer(DbTableStorage table, Action finalizer)
-        : base(table, finalizer)
-      {
-      }
-
-      protected override void CreateStreams()
-      {
-        _indexStream = _table.OpenWrite(_table._indexName);
-        try
-        {
-          _readStream = _table.OpenWrite(_table._dataName, true);
-          _writeStream = _readStream;
+            _lock.EnterReadLock();
+            try
+            {
+                return new Reader(this, _lock.ExitReadLock);
+            }
+            catch
+            {
+                _lock.ExitReadLock();
+                throw;
+            }
         }
-        catch
+
+        public IDbTableWriter BeginWrite()
         {
-          _indexStream.Dispose();
-          throw;
+            _lock.EnterWriteLock();
+            try
+            {
+                return new Writer(this, _lock.ExitWriteLock);
+            }
+            catch
+            {
+                _lock.ExitWriteLock();
+                throw;
+            }
         }
-      }
 
-      public DateTimeOffset WriteIndex(byte[] data, int length)
-      {
-        _indexStream.Position = 0;
-        _indexStream.Write(data, 0, length);
-        _indexStream.SetLength(length);
-        _indexStream.Dispose();
+        public IDbTableWriter BeginCompact()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                return new Compacter(this, _lock.ExitWriteLock);
+            }
+            catch
+            {
+                _lock.ExitWriteLock();
+                throw;
+            }
+        }
 
-        UpdateTs();
+        class Reader : IDbTableReader
+        {
+            protected Stream _readStream, _indexStream;
+            protected readonly DbTableStorage _table;
+            readonly Action _finalizer;
+            internal DateTimeOffset _ts;
 
-        return _ts;
-      }
+            public Reader(DbTableStorage table, Action finalizer)
+            {
+                _table = table;
+                _finalizer = finalizer;
+                CreateStreams();
+                UpdateTs();
+            }
 
-      public void CopyData(long position, long target, int length)
-      {
-        if (_readStream.Position != position)
-          _readStream.Seek(position, SeekOrigin.Begin);
+            protected void UpdateTs()
+            {
+                _ts = File.GetLastWriteTime(_table._indexName);
+            }
 
-        var buffer = ReadStream(_readStream, length);
+            protected virtual void CreateStreams()
+            {
+                _indexStream = _table.OpenRead(_table._indexName);
+                try
+                {
+                    _readStream = _table.OpenRead(_table._dataName, true);
+                }
+                catch
+                {
+                    _indexStream.Dispose();
+                    throw;
+                }
+            }
 
-        if (_writeStream.Position != target)
-          _writeStream.Seek(target, SeekOrigin.Begin);
+            public DateTimeOffset Ts { get { return _ts; } }
 
-        _writeStream.Write(buffer, 0, length);
-      }
+            public DbTableInfo GetInfo()
+            {
+                return new DbTableInfo
+                {
+                    DataSize = _readStream.Length,
+                    IndexSize = _indexStream.Length
+                };
+            }
 
-      public void WriteData(byte[] data, long position, int length)
-      {
-        if (_writeStream.Position != position)
-          _writeStream.Seek(position, SeekOrigin.Begin);
+            public byte[] ReadIndex()
+            {
+                int len = (int)_indexStream.Length;
+                if (len == 0)
+                    return null;
 
-        _writeStream.Write(data, 0, length);
-      }
+                return ReadStream(_indexStream, len);
+            }
 
-      public void Purge()
-      {
-        _indexStream.SetLength(0);
-        _writeStream.SetLength(0);
-      }
+            protected static byte[] ReadStream(Stream stream, int len)
+            {
+                var buffer = new byte[len];
 
-      public void CropData(long size)
-      {
-        _writeStream.SetLength(size);
-      }
+                var pos = 0;
+                while (pos < len)
+                    pos += stream.Read(buffer, pos, len - pos);
 
-      public override void Dispose()
-      {
-        _writeStream.Dispose();
-        base.Dispose();
-      }
+                return buffer;
+            }
+
+            public byte[] ReadData(long position, int length)
+            {
+                if (_readStream.Position != position)
+                    _readStream.Seek(position, SeekOrigin.Begin);
+
+                return ReadStream(_readStream, length);
+            }
+
+            public virtual void Dispose()
+            {
+                _readStream.Dispose();
+                _indexStream.Dispose();
+
+                _finalizer();
+            }
+        }
+
+        class Writer : Reader, IDbTableWriter
+        {
+            Stream _writeStream;
+
+            public Writer(DbTableStorage table, Action finalizer)
+              : base(table, finalizer)
+            {
+            }
+
+            protected override void CreateStreams()
+            {
+                _indexStream = _table.OpenWrite(_table._indexName);
+                try
+                {
+                    _readStream = _table.OpenWrite(_table._dataName, true);
+                    _writeStream = _readStream;
+                }
+                catch
+                {
+                    _indexStream.Dispose();
+                    throw;
+                }
+            }
+
+            public DateTimeOffset WriteIndex(byte[] data, int length)
+            {
+                _indexStream.Position = 0;
+                _indexStream.Write(data, 0, length);
+                _indexStream.SetLength(length);
+                _indexStream.Dispose();
+
+                UpdateTs();
+
+                return _ts;
+            }
+
+            public void CopyData(long position, long target, int length)
+            {
+                if (_readStream.Position != position)
+                    _readStream.Seek(position, SeekOrigin.Begin);
+
+                var buffer = ReadStream(_readStream, length);
+
+                if (_writeStream.Position != target)
+                    _writeStream.Seek(target, SeekOrigin.Begin);
+
+                _writeStream.Write(buffer, 0, length);
+            }
+
+            public void WriteData(byte[] data, long position, int length)
+            {
+                if (_writeStream.Position != position)
+                    _writeStream.Seek(position, SeekOrigin.Begin);
+
+                _writeStream.Write(data, 0, length);
+            }
+
+            public void Purge()
+            {
+                _indexStream.SetLength(0);
+                _writeStream.SetLength(0);
+            }
+
+            public void CropData(long size)
+            {
+                _writeStream.SetLength(size);
+            }
+
+            public override void Dispose()
+            {
+                _writeStream.Dispose();
+                base.Dispose();
+            }
+        }
+
+        class Compacter : Writer
+        {
+            public Compacter(DbTableStorage table, Action finalizer)
+              : base(MoveFile(table), finalizer)
+            {
+                _readStream = _table.OpenRead(GetBackupName(_table), true);
+            }
+
+            static DbTableStorage MoveFile(DbTableStorage table)
+            {
+                var backup = GetBackupName(table);
+
+                if (File.Exists(backup))
+                    File.Delete(backup);
+
+                if (File.Exists(table._dataName))
+                    File.Move(table._dataName, backup);
+
+                return table;
+            }
+
+            static string GetBackupName(DbTableStorage table)
+            {
+                return table._dataName + ".bak";
+            }
+
+            public override void Dispose()
+            {
+                base.Dispose();
+
+                File.Delete(GetBackupName(_table));
+            }
+        }
     }
-
-    class Compacter : Writer
-    {
-      public Compacter(DbTableStorage table, Action finalizer)
-        : base(MoveFile(table), finalizer)
-      {
-        _readStream = _table.OpenRead(GetBackupName(_table), true);
-      }
-
-      static DbTableStorage MoveFile(DbTableStorage table)
-      {
-        var backup = GetBackupName(table);
-
-        if (File.Exists(backup))
-          File.Delete(backup);
-
-        if (File.Exists(table._dataName))
-          File.Move(table._dataName, backup);
-
-        return table;
-      }
-
-      static string GetBackupName(DbTableStorage table)
-      {
-        return table._dataName + ".bak";
-      }
-
-      public override void Dispose()
-      {
-        base.Dispose();
-
-        File.Delete(GetBackupName(_table));
-      }
-    }
-  }
 }
