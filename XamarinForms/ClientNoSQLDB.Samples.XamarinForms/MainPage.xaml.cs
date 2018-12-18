bbabce6349a19b670e928cc078d1161914f68143
Ken Tucker <vb2ae@live.com>
2018-12-18 07:19:56 -0500

    Working on sample apps

--- XamarinForms/ClientNoSQLDB.Samples.XamarinForms/MainPage.xaml.cs
+++ XamarinForms/ClientNoSQLDB.Samples.XamarinForms/MainPage.xaml.cs
@@ -12,6 +12,38 @@ namespace ClientNoSQLDB.Samples.XamarinForms
         public MainPage()
         {
             InitializeComponent();
+            List<Person> list;
+            using (var db = new ClientNoSqlDB.DbInstance("testing"))
+            {
+                db.Map<Person>().Automap(i => i.Id);
+                db.Initialize();
+                list = db.LoadAll<Person>().ToList();
+                if (list != null && !list.Any())
+                {
+                    db.Save(new Person() { FirstName = "Ken", Id = 1, LastName = "Tucker" },
+                    new Person() { FirstName = "Tony", Id = 2, LastName = "Stark" },
+                    new Person() { FirstName = "John", Id = 3, LastName = "Papa" },
+                    new Person() { FirstName = "Delete", Id = 4, LastName = "Me" });
+                }
+
+                var item = db.LoadByKey<Person>(4);
+                if (item != null)
+                {
+                    db.Delete<Person>(item);
+                }
+                list = db.LoadAll<Person>().ToList();
+            }
+            lstNames.ItemsSource = list;
         }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
     }
 }
